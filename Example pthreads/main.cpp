22ebc5a0b778b7e6aaa5043821e99032a77a067f
Laura <l4depende@gmail.com>
2016-02-29 10:52:33 +0000

    Initial Commit

---
+++ Example pthreads/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Example pthreads
+//
+//  Created by Laura del Pino Díaz on 29/2/16.
+//  Copyright © 2016 Laura del Pino Díaz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
